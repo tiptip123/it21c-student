77d5e75124343057798d059766ad8bf045fc27e5
tiptip123 <josephbaguio327@gmail.com>
2023-09-28 19:49:45 +0800

    Code

---
+++ src/Student.java
@@ -0,0 +1,42 @@
+
+public class Student {
+
+    String name, section, motto;
+
+    String name() {
+
+        return name;
+
+    }
+
+    String getSection() {
+
+        return section;
+
+    }
+
+    String getMotto() {
+
+        return motto;
+
+    }
+
+    void setName(String newName) {
+
+        name = newName;
+
+    }
+
+    void setSection(String newSection) {
+
+        section = newSection;
+
+    }
+
+    void setMotto(String newMotto) {
+
+        motto = newMotto;
+
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,21 @@
+
+public class main {
+   
+    
+
+    
+    public static void main(String [] args){
+    
+   Student student = new Student();
+    student.setName("joseph Jay Baguio");
+     student.setSection("it21C");
+     student.setMotto("susuka peru di susuko");
+    
+    
+    
+        System.out.println("Name: "+ student.name);
+        System.out.println("Section: "+ student.section);
+        System.out.println("Motto: "+ student.motto);
+    
+    }    
+}
